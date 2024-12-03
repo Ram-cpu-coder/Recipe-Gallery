5afa0133dd4407d29e17845b11d731f13e1c9a7c
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2024-12-03 12:23:00 +1100

    search

--- src/IngredientsAndProcess.jsx
+++ src/IngredientsAndProcess.jsx
@@ -1,29 +1,35 @@
 import React from "react";
+import "./Scrollbar.css";
 
 const IngredientsAndProcess = ({ item, closeModal }) => {
-  const { name, ingredients, process } = item;
+  const { name, ingredients, process, image } = item;
 
   return (
-    <div className="fixed inset-0 z-50 flex flex-col justify-center items-center min-h-[100vh] w-[100vw] bg-white text-black">
-      <h1 className="mb-[100px] font-bold text-4xl tracking-wide">{name}</h1>
-      <div className="flex justify-center items-start h-[auto]">
-        <div className="flex flex-col items-start w-[350px] gap-3">
-          <p className="font-semibold">Ingredients:</p>
-          <ul>
-            {ingredients.map((item, index) => {
-              return <li key={index}>• {item}</li>;
-            })}
-          </ul>
-        </div>
-        <div className="w-[400px] gap-3 flex flex-col text-center relative">
-          <div className="font-semibold">Let's Prepare Now</div>
-          <div>{process}</div>
-          <button
-            className=" fixed top-10 right-10 rounded-lg bg-red-600 hover:bg-red-400 active:bg-red-500 focus:outline-none focus:ring focus:ring-violet-300 text-white text-xl px-4 py-2 "
-            onClick={() => closeModal(false)}
-          >
-            🗙
-          </button>
+    <div
+      style={{ backgroundImage: `url(${image})` }}
+      className="background-ingredients fixed inset-0 z-50 flex flex-col justify-center items-center min-h-[100vh] w-[100vw]"
+    >
+      <div className="bg-white opacity-80 inset-0 fixed flex flex-col justify-center items-center min-h-[100vh] w-[100vw] text-black">
+        <h1 className="mb-[100px] font-bold text-4xl tracking-wide">{name}</h1>
+        <div className="flex justify-center items-start h-[auto]">
+          <div className="flex flex-col items-start w-[350px] gap-3">
+            <p className="font-semibold text-2xl">Ingredients:</p>
+            <ul className="text-xl">
+              {ingredients.map((item, index) => {
+                return <li key={index}>• {item}</li>;
+              })}
+            </ul>
+          </div>
+          <div className="w-[400px] gap-3 flex flex-col text-center relative">
+            <div className="font-semibold text-2xl">Let's Prepare Now</div>
+            <div className="text-xl">{process}</div>
+            <button
+              className=" fixed top-10 right-10 rounded-lg bg-red-600 hover:bg-red-400 active:bg-red-500 focus:outline-none focus:ring focus:ring-violet-300 text-white text-xl px-4 py-2 "
+              onClick={() => closeModal(false)}
+            >
+              🗙
+            </button>
+          </div>
         </div>
       </div>
     </div>
